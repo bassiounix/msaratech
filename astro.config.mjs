b77bd21a8a9000aab99f53680c98c76ab0e727d1
Muhammad Bassiouni <bassiouni.git@gmail.com>
2025-02-19 00:45:31 +0200

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://msaratech.com',
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  })
+});
